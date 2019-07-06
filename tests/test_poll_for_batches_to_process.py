a8a58b80378a9d5d883227b882f993f1b575b4cb
HannahKent <hkent@acrotron.com>
2019-07-05 21:48:03 -0600

    SDC-529 Unit test coverage increased to 98%

--- tests/test_poll_for_batches_to_process.py
+++ tests/test_poll_for_batches_to_process.py
@@ -4,6 +4,7 @@ import os
 import time
 import boto3
 import pytest
+import json
 sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
 from lambdas.poll_for_batches_to_process_handler import SqsHandler
 
@@ -43,31 +44,69 @@ def test_poll_for_batches_historical():
         poll_batches_to_process_obj.poll_for_batches(queue_event, None)
 
 
-# @mock_sqs
-# def test_poll_for_batches_not_historical(monkeypatch):
-#     sqs = boto3.client('sqs', region_name='us-east-1')
-#     response = sqs.create_queue(QueueName='dev-dot-sdc-waze-data-persistence-orchestration',
-#                                 Attributes={'FifoQueue': "false", 'DelaySeconds': "5", 'MaximumMessageSize': "262144",
-#                                             'MessageRetentionPeriod': "1209600", 'VisibilityTimeout': "960"})
-#     queue_url = response['QueueUrl']
-#
-#     queue_name = queue_url[queue_url.rfind('/') + 1: len(queue_url)]
-#     os.environ["persistence_sqs"] = queue_name
-#     queue_event = dict()
-#     queue_event["is_historical"] = "false"
-#     batchId = str(int(time.time()))
-#     queue_event["BatchId"] = batchId
-#     poll_batches_to_process_obj = SqsHandler()
-#     def mock_get_dict():
-#         data = dict()
-#         data["BatchId"] = batchId
-#         return data
-#     message = {"BatchId": batchId, "Status": "Manifest generation completed"}
-#     monkeypatch.setattr('lambdas.poll_for_batches_to_process_handler.SqsHandler.poll_for_batches', mock_get_dict())
-#     monkeypatch.setattr('lambdas.poll_for_batches_to_process_handler.SqsHandler.publish_message_to_sns', message)
-#     poll_batches_to_process_obj.poll_for_batches(queue_event, None)
-#
-#     assert True
+@mock_sqs
+def test_poll_for_batches_historical_status_assigned(monkeypatch):
+
+    def mock_publish_message(*args, **kwargs):
+        pass
+
+    monkeypatch.setattr(SqsHandler, "publish_message_to_sns", mock_publish_message)
+    os.environ["persistence_sqs"] = "dev-dot-sdc-curated-batches.fifo"
+    sqs = boto3.resource('sqs', region_name='us-east-1')
+    sqs.create_queue(QueueName=os.environ["persistence_sqs"])
+    queue_event = dict()
+    queue_event["is_historical"] = "false"
+    queue_event["BatchId"] = str(int(time.time()))
+    poll_batches_to_process_obj = SqsHandler()
+
+    data = poll_batches_to_process_obj.poll_for_batches(queue_event, None)
+    assert data["is_historical"] == queue_event["is_historical"].lower()
+
+
+def test_poll_for_batches_batches_not_in_event(monkeypatch):
+
+    class MockMessage:
+        body = None
+
+    class MockQueue:
+        @staticmethod
+        def receive_messages(*args, **kwargs):
+            mock_messages = [MockMessage()]
+            mock_messages[0].queue_url = "test_queue_url"
+            mock_messages[0].receipt_handle = "test_receipt_handle"
+            return mock_messages
+
+    class MockSQS:
+        @staticmethod
+        def get_queue_by_name(*args, **kwargs):
+            queue = MockQueue()
+            return queue
+
+    def mock_boto3_resource(*args, **kwargs):
+        sqs = MockSQS()
+        return sqs
+
+    def mock_json_loads(*args, **kwargs):
+        return {"BatchId": "test_batch_id"}
+
+    def mock_publish_message(*args, **kwargs):
+        pass
+
+    monkeypatch.setattr(boto3, "resource", mock_boto3_resource)
+    monkeypatch.setattr(json, "loads", mock_json_loads)
+    monkeypatch.setattr(SqsHandler, "publish_message_to_sns", mock_publish_message)
+
+    os.environ["persistence_sqs"] = "dev-dot-sdc-curated-batches.fifo"
+    boto3.setup_default_session()
+    queue_event = dict()
+    queue_event["is_historical"] = "false"
+    poll_batches_to_process_obj = SqsHandler()
+
+    data = poll_batches_to_process_obj.poll_for_batches(queue_event, None)
+
+    assert data["BatchId"] == "test_batch_id"
+    assert data["queueUrl"] == "test_queue_url"
+    assert data["receiptHandle"] == "test_receipt_handle"
 
 
 @mock_events
